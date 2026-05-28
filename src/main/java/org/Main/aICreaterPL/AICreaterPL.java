06a491ae1f572f61467420a95bfbbe7d7099928c
1 <notniy9@gmail.com>
2026-05-28 23:24:46 +0600

    first commit

---
+++ src/main/java/org/Main/aICreaterPL/AICreaterPL.java
@@ -0,0 +1,17 @@
+package org.Main.aICreaterPL;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AICreaterPL extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
